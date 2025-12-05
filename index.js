39aeb5efbc3674b024dd3e3da0b9faef9ef010d8
Porter <pdhakes25@gmail.com>
2025-12-04 17:31:06 -0700

    added reset and randomized background change

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  const randomNumber = () => Math.floor(Math.random() * 256)
   const bodyColor = document.querySelector("body")
-  bodyColor.style.backgroundColor = "rgb(10, 71, 6)"
-
+  bodyColor.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  const bodyColor = document.querySelector("body")
+  bodyColor.style.backgroundColor = ""
 }
 
 // Capture Keyboard Input
@@ -18,6 +20,7 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+
 }
 
 // Process Text Input
